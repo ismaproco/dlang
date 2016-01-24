0be9874995f317c98dcf52517f3aea46906d469a
Ismael <ismaproco@gmail.com>
2016-01-24 21:10:30 +0100

    get filename from the arguments

--- io/readFile.d
+++ io/readFile.d
@@ -4,11 +4,27 @@ import std.file;
 
 // Main function
 int main(char[][] args)
-{    
-  string filename = "text.txt";
-  if (exists(filename)!=0) {
-    ulong size = getSize(filename);    
-    writefln("%s", cast(char[])read(filename));
+{ 
+
+  //get the filename from the input args
+  if(args.length < 2) {
+      writefln("add a filename as input parameter in the command line");
+      writefln("ex: ./readFile test.txt");
+      //exit safely
+      return 0;
+  }
+
+  //define a variable to manage the name of the file
+  string filename = cast(string)args[1];
+  //exits?
+  if (exists(filename) != 0 ) {
+    // get the size of the file
+    ulong size = getSize(filename);
+
+    //get the file string data
+    string data = cast(string)read(filename);
+
+    writefln("%s", data );
     writefln("");
     writefln("Size of file = %d (bytes).",size);
   } else {
