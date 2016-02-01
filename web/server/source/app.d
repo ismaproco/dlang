e474382e14bdb20195b9048b444b474a0e9a403d
Ubuntu <ubuntu@ip-172-31-43-85.us-west-2.compute.internal>
2016-02-01 13:35:02 +0000

    updated routes for the static web browser

--- web/server/source/app.d
+++ web/server/source/app.d
@@ -2,15 +2,18 @@ import vibe.d;
 
 shared static this()
 {
+	// Create the router that will dispatch each request to the proper handler method
+	auto router = new URLRouter;
+	
+	// All requests that haven't been handled by the web interface registered above
+	// will be handled by looking for a matching file in the public/ folder.
+	router.get("*", serveStaticFiles("public/"));
+	router.get("/", serveStaticFiles("public/index.html"));
+	
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
-
+	
+	listenHTTP(settings, router);
 	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
-}
-
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
 }
