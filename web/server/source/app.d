cfa59fddb8474e697b77c66fcf499c04a6d55df0
Ubuntu <ubuntu@ip-172-31-43-85.us-west-2.compute.internal>
2016-02-01 14:53:33 +0000

    Added comments, and set the parameters to load the static server in all networks

--- web/server/source/app.d
+++ web/server/source/app.d
@@ -3,6 +3,7 @@ import vibe.http.fileserver;
 import vibe.http.router;
 import vibe.http.server;
 
+// method that manage the root request of the router
 void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res)
 {
 	res.redirect("/index.html");
@@ -10,15 +11,23 @@ void handleRequest(scope HTTPServerRequest req, scope HTTPServerResponse res)
 
 shared static this()
 {
+	// configure the setting of the web server
 	auto settings = new HTTPServerSettings;
 	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
+	// using the address in this way it will listen in all networks, 
+	//but it will show a failed messafe in the console
+	settings.bindAddresses = ["::", "0.0.0.0"];
 
+	//router configuration
 	auto router = new URLRouter;
+	//when the root is requested executes the handleRequest function
 	router.get("/", &handleRequest);
+	//configure the custom enconding for the compressed files
 	auto fileServerSettings = new HTTPFileServerSettings;
 	fileServerSettings.encodingFileExtension = ["gzip" : ".gz"];
 	router.get("/gzip/*", serveStaticFiles("./public/", fileServerSettings));
+	
+	// set the location of the default files in the public folder
 	router.get("*", serveStaticFiles("./public/",));
 
 	listenHTTP(settings, router);
